570c46266ae9203c72995a1f487f6112c0e7f201
David Denton <denton.david@gmail.com>
2016-08-30 20:32:29 +0100

    dave & maya - adding page

---
+++ js/lib.js
@@ -0,0 +1,13 @@
+
+function homePageLoad() {
+    $('#submit')
+        .click(function () {
+            $.cookie("name", $('#name').val());
+            $.cookie("password", $('#password').val());
+            window.location = "sortinghat.html";
+        });
+}
+
+function sortingHatLoad() {
+    $('#greeting').append("hello " + $.cookie('name'));
+}
